7ba19796700eb6cacf5d373aab98c7e600570235
Adam Koniar <akoniar@redhat.com>
2022-06-27 14:05:19 +0200

    Init commit

---
+++ src/main/java/io/vertx/qe/kafka/MainVerticle.java
@@ -0,0 +1,12 @@
+package io.vertx.qe.kafka;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+
+    }
+
+}
